0847bd87f70afeef61c60bf9fd05219c7abce80c
HrishikShaji <hrishikminishaji@gmail.com>
2023-09-11 20:54:34 +0530

    masktext added

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,20 @@
-import { CursorTrailer } from "@/components/CursorTrailer";
-import Image from "next/image";
+import { CursorTrailer } from '@/components/CursorTrailer'
+import { MaskText } from '@/components/MaskText'
+import Image from 'next/image'
 
 export default function Home() {
+  const phrases = [
+    'It is a long established fact',
+    'that a reader will be distracted',
+    'by the readable content of a page',
+    'when looking at its layout',
+  ]
   return (
-    <main className="bg-neutral-900">
-      <CursorTrailer />
+    <main className='bg-white text-black flex flex-col items-center gap-20 mt-20'>
+      <MaskText phrases={phrases} />
+      <MaskText phrases={phrases} />
+      <MaskText phrases={phrases} />
+      <MaskText phrases={phrases} />
     </main>
-  );
+  )
 }
---
+++ components/MaskText.tsx
@@ -0,0 +1,43 @@
+'use client'
+import { useInView, motion } from 'framer-motion'
+import React, { useEffect, useRef } from 'react'
+
+interface MaskTextProps {
+  phrases: Record<string, any>
+}
+
+export const MaskText: React.FC<MaskTextProps> = ({ phrases }) => {
+  const body = useRef(null)
+  const isInView = useInView(body, { once: true, margin: '-10%' })
+
+  useEffect(() => {
+    console.log(isInView)
+  }, [isInView])
+
+  const animate = {
+    initial: { y: '100%' },
+    open: (i) => ({
+      y: '0%',
+      transition: { duration: 0.5, delay: 0.1 * i, ease: [0.33, 1, 0.68, 1] },
+    }),
+  }
+
+  return (
+    <div ref={body} className='text-3xl font-bold'>
+      {phrases.map((phrase, i) => (
+        <div className='overflow-hidden' key={i}>
+          <motion.p
+            variants={animate}
+            custom={i}
+            initial='initial'
+            animate={isInView ? 'open' : ''}
+            key={i}
+            className='font-bold '
+          >
+            {phrase}
+          </motion.p>
+        </div>
+      ))}
+    </div>
+  )
+}
