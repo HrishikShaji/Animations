9deb4d5b74f036ca3ca136b089a44d43f6194225
HrishikShaji <hrishikminishaji@gmail.com>
2023-09-23 11:55:37 +0530

    list animation done

--- components/GsapDemoTwo.tsx
+++ components/GsapDemoTwo.tsx
@@ -16,26 +16,50 @@ export const GsapDemoTwo = () => {
       .timeline()
       .from(headingOne, { duration: 1, opacity: 0 })
       .from(headingTwo, { opacity: 0, scale: 0, ease: 'back' })
-      .from(headingThree, { y: 160, duration: 0.8, ease: 'back' })
+      .from(headingThree, { y: 160, duration: 0.8, ease: 'back' }, '+=1')
       .from(headingFour, { xPercent: 100, duration: 0.2 })
-      .from(headingFive, { yPercent: 200, duration: 1 })
+      .from(headingFive, { yPercent: 200, duration: 1 }, '<')
   }, [])
 
-  const manageMouseEnter = () => {
+  const manageMouseHover = () => {
     gsap.fromTo(
       circleRef.current,
       { scale: 0 },
       {
-        scale: 0,
+        scale: 1,
         duration: 1,
         ease: 'circ'
       }
     )
   }
+
+  const items = [
+    {
+      name: 'ONE'
+    },
+    {
+      name: 'TWO'
+    },
+    {
+      name: 'THREE'
+    }
+  ]
+  const manageMouseEnter = (id: number) => {
+    const title = document.getElementById(`k_${id}`)
+    const dot = document.getElementById(`j_${id}`)
+    gsap.to(title, { x: 10 })
+    gsap.to(dot, { scale: 2 })
+  }
+  const manageMouseLeave = (id: number) => {
+    const title = document.getElementById(`k_${id}`)
+    const dot = document.getElementById(`j_${id}`)
+    gsap.to(title, { x: 0 })
+    gsap.to(dot, { scale: 1 })
+  }
   return (
-    <div className='h-screen w-full flex flex-col gap-10 justify-center text-white items-center'>
+    <div className='h-screen w-full flex flex-col gap-10 p-10 justify-center text-white items-center'>
       <div
-        onMouseEnter={manageMouseEnter}
+        onMouseEnter={manageMouseHover}
         className='h-40 w-40 bg-teal-400 flex justify-center items-center rounded-full relative'
       >
         <div
@@ -60,6 +84,21 @@ export const GsapDemoTwo = () => {
         <h1 id='headingFive' className=''>
           A WEB DEVELOPER
         </h1>
+      </div>
+      <div className='flex flex-col gap-2 w-full items-start'>
+        {items.map((item, i) => (
+          <div
+            className='flex gap-2 items-center'
+            key={i}
+            onMouseEnter={() => manageMouseEnter(i)}
+            onMouseLeave={() => manageMouseLeave(i)}
+          >
+            <div id={`j_${i}`} className='h-2 w-2 rounded-full bg-teal-500' />
+            <h3 id={`k_${i}`} className='font-semibold text-xl'>
+              {item.name}
+            </h3>
+          </div>
+        ))}
       </div>
     </div>
   )
