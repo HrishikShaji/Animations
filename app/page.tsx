43f7f2ae650460691351238c5fb010a1ab8697d8
HrishikShaji <hrishikminishaji@gmail.com>
2023-09-23 17:55:45 +0530

    horizontal scroll done

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 'use client'
 
 import { GsapGallery } from '@/components/GsapGallery'
+import { HorizontalScroll } from '@/components/HorizontalScroll'
 
 export default function Home() {
   return (
     <main className=''>
       <GsapGallery />
+      <HorizontalScroll />
     </main>
   )
 }
---
+++ components/HorizontalScroll.tsx
@@ -0,0 +1,69 @@
+'use client'
+import gsap from 'gsap'
+import ScrollTrigger from 'gsap/dist/ScrollTrigger'
+import { useEffect, useRef } from 'react'
+
+export const HorizontalScroll = () => {
+  const targetRef = useRef(null)
+  const lineRef = useRef(null)
+  useEffect(() => {
+    gsap.registerPlugin(ScrollTrigger)
+
+    const sections = gsap.utils.toArray('section')
+    let scrollTween = gsap.to(sections, {
+      xPercent: -100 * (sections.length - 1),
+      ease: 'none',
+      scrollTrigger: {
+        trigger: targetRef.current,
+        pin: true,
+        scrub: 1,
+        end: '+=3000'
+      }
+    })
+
+    gsap.to(lineRef.current, {
+      width: '80%',
+      scrollTrigger: {
+        trigger: targetRef.current,
+        start: 'top left',
+        scrub: 1
+      }
+    })
+
+    sections.forEach((section) => {
+      let square = section.querySelectorAll('div')
+
+      gsap.from(square, {
+        y: -100,
+        opacity: 0,
+        duration: 2,
+        ease: 'elastic',
+        stagger: 0.1,
+        scrollTrigger: {
+          trigger: section,
+          containerAnimation: scrollTween,
+          start: 'left center'
+        }
+      })
+    })
+  }, [])
+  return (
+    <div ref={targetRef} className='overflow-x-hidden relative box-border'>
+      <div
+        ref={lineRef}
+        className='absolute top-10 z-20 left-10 w-[200px] h-1 bg-white'
+      />
+      <div className='flex w-[300vw]'>
+        <section className='w-screen h-screen bg-neutral-900 flex justify-center items-center'>
+          <div className='bg-teal-500 w-20 h-20 rounded-md' />
+        </section>
+        <section className='w-screen h-screen bg-neutral-800 flex justify-center items-center'>
+          <div className='bg-teal-500 w-20 h-20 rounded-md' />
+        </section>
+        <section className='w-screen h-screen bg-neutral-700 flex justify-center items-center'>
+          <div className='bg-teal-500 w-20 h-20 rounded-md' />
+        </section>
+      </div>
+    </div>
+  )
+}
